e4f4c7edef7ad3d45f77cb06b3d211dc302af900
Katlego-Bruce <85530704+Katlego-Bruce@users.noreply.github.com>
2026-04-06 09:56:46 +0200

    Delete apps directory

--- apps/api/src/entities/index.ts
+++
@@ -1,7 +0,0 @@
-export { User } from './user.entity';
-export { Organization } from './organization.entity';
-export { Job } from './job.entity';
-export { Wallet } from './wallet.entity';
-export { LedgerEntry } from './ledger-entry.entity';
-export { Escrow } from './escrow.entity';
-export { EscrowEvent } from './escrow-event.entity';
--- apps/web/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['@ispani/ui', '@ispani/shared'],
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
