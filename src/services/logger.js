e9be3b544af90e8ee707c07e1806e286c4de0a72
Sese Ramokgadi <ramokgadiks@gmail.com>
2026-04-07 13:30:56 +0200

    feat: add complete Winston enterprise logging service

--- src/services/logger.js
+++ src/services/logger.js
@@ -2,7 +2,6 @@ const winston = require('winston');
 const path = require('path');
 const fs = require('fs');
 
-// Create logs directory if it doesn't exist
 const logsDir = path.join(__dirname, '../../logs');
 if (!fs.existsSync(logsDir)) {
   fs.mkdirSync(logsDir, { recursive: true });
